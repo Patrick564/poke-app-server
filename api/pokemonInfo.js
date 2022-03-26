d2fcc3936eb3144537954e3084f195f81204ed46
Patrick Vilchez <pvilchez794@gmail.com>
2022-03-26 14:55:25 -0500

    Change to request api

--- api/pokemonInfo.js
+++ api/pokemonInfo.js
@@ -9,16 +9,16 @@ const getPokemonInfo = async ({ pokemon = 'ditto' }) => {
       front_default: frontDefault,
       front_female: frontFemale,
       front_shiny: frontShiny
-    } = info.data.sprites
+    } = await info.data.sprites
 
     // Get an object with stats and names
-    const stats = info.data.stats.map((stat) => ({ baseStat: stat.base_stat, name: stat.stat.name }))
+    const stats = await info.data.stats.map((stat) => ({ baseStat: stat.base_stat, name: stat.stat.name }))
 
     // Get an array of types
-    const types = [...info.data.types.map((type) => type.type.name)]
+    const types = [...await info.data.types.map((type) => type.type.name)]
 
-    const id = info.data.id
-    const name = info.data.name
+    const id = await info.data.id
+    const name = await info.data.name
 
     return { frontDefault, frontFemale, frontShiny, stats, types, id, name }
   } catch (error) {
@@ -26,4 +26,4 @@ const getPokemonInfo = async ({ pokemon = 'ditto' }) => {
   }
 }
 
-module.exports = getPokemonInfo
+exports.handler = getPokemonInfo
